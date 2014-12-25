a9e6a5ae60f3560002c8893278440427bf16c137
caesor <liaozksysu@gmail.com>
2014-12-25 23:27:21 +0800

    version1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+mobile-gallary-demo
+===================
---
+++ js/common.js
@@ -0,0 +1,15 @@
+var checkForm = {
+	dis_warn:function(obj){
+		obj.removeClass("form_warning");
+	},
+	check_warn:function(obj){
+		var suc = true;
+		obj.find(".check_required").each(function(){
+			if( $(this).val() == '' ){
+				$(this).addClass("form_warning");
+				suc = false;
+			}
+		})
+		return suc;
+	}
+}
